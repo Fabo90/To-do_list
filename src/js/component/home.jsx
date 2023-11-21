699bcec25ca71ce86dbe83d551265b1d50c327c8
Fabo90 <139248863+Fabo90@users.noreply.github.com>
2023-11-21 05:23:15 +0000

    Fabian to-do list

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
-//create your first component
 const Home = () => {
+	const [inputValue, setImputValue] = useState("");
+	const [list, setList] = useState([]);
+	const [style, setStyle] = useState({display: 'none'});
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+		<div className="small-middle-container">
+			<h1 className="text-center text-secondary display-5">TO-DO LIST</h1>
+			<ul class="list-group">
+				<input 
+				type="text" 
+				className="form-control" 
+				placeholder="What needs to be done?"
+				onChange={(e) => setImputValue(e.target.value)}
+				value={inputValue}
+				onKeyDown={(e) => {
+					if (e.key === "Enter" && inputValue != "") {
+						setList(list.concat(inputValue));
+						setImputValue("");
+					} else if (e.key === "Enter" && inputValue == "") {
+						alert ("Please write something!");
+					};
+				}}
+				></input>
+				<div>
+					{list.map((ele, index) => (
+						<li className="form-control"
+							onMouseOver={() => {
+                     			setStyle({display: 'block'});
+                 			}}
+                 			onMouseLeave={() => {
+                     			setStyle({display: 'none'});
+                			}}>
+							{ele} 
+							<button className="btn-close float-end"
+									onClick={() =>
+										setList(list.filter((i, ind) => index != ind))}
+									style={style}
+							></button>
+						</li>
+					))}
+				</div>
+			</ul>
+			<div> 
+				{list.length == "" ? "Nothing to do, please add something to the list..." : list.length + " tasks"}
+			</div>
+		</div>	
 	);
 };
-
 export default Home;
